b47ac599ff06e9c12520816e3a96d9c911bce728
AleksaCamilovic <aleksa.camilovic@outlook.com>
2019-07-29 14:26:08 +0200

    apollo package, gql queries, categories and books

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,115 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApolloQuery :query="categoriesQuery">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('all')">All</a>
+          <a href="#" class="link-margin" @click.prevent="selectCategory('featured')">Featured</a>
+          <a href="#" v-for="category of data.categories" :key="category.id" class="user link-margin"
+          @click.prevent="selectCategory(category.id)"
+          >
+            {{ category.id }}. {{ category.name }}
+          </a>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    <div v-if="selectedCategory === 'all'">
+      <ApolloQuery :query="query">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.books" :key="book.id" class="user link-margin">
+              {{ book.id }}. {{ book.title }} / {{ book.author }}
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else-if="selectedCategory === 'featured'">
+      <ApolloQuery :query="query" :variables="{ featured: true }">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.booksByFeatured" :key="book.id">
+              {{ book.id }}. {{ book.title }}
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
+    <div v-else>
+      <ApolloQuery :query="query" :variables="{id: selectedCategory}">
+        <template slot-scope="{ result: { data, loading }, isLoading }">
+          <div v-if="isLoading">Loading...</div>
+          <div v-else>
+            <div v-for="book of data.category.books" :key="book.id" class="user link-margin">
+              {{ book.id }}. {{ book.title }}
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import gql from 'graphql-tag'
+import categoryQuery from '@/graphql/queries/category.gql'
+import booksQuery from '@/graphql/queries/books.gql'
+import categoriesQuery from '@/graphql/queries/categories.gql'
+import booksFeaturedQuery from '@/graphql/queries/booksFeatured.gql'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      categoryQuery,
+      categoriesQuery,
+      booksFeaturedQuery,
+      booksQuery,
+      categories: [],
+      selectedCategory: 'all',
+      query: booksQuery
+    }
+  },
+  // apollo: {
+  //   categories: gql`{
+  //     categories {
+  //       id
+  //       name
+  //     }
+  //   }`,
+  // },
+  methods: {
+    selectCategory(category) {
+      if(category === 'all') {
+        this.query = booksQuery
+      } else if(category === 'featured') {
+        this.query = booksFeaturedQuery
+      } else {
+        this.query = categoryQuery
+      }
+      this.selectedCategory = category
+    }
   }
 }
 </script>
+
+<style scoped>
+
+.link-margin {
+  margin-right: 24px;
+}
+
+</style>
+
